96f2bc152a5741bd38ad83162652cb6be50e7bf4
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 09:58:19 -0500

    finally got infinite scroll to work.. holy fuck

--- home/js/main.js
+++ home/js/main.js
@@ -22,34 +22,16 @@ $(function(){
 	    columnWidth : 240
 	  });
 	});
-
-    $container.infinitescroll({
-      navSelector  : '#page-nav',    // selector for the paged navigation 
-      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.item',     // selector for all items you'll retrieve
-      loading: {
-          finishedMsg: 'No more pages to load.',
-          img: 'http://i.imgur.com/6RMhx.gif'
-        }
-      },
-      
-      // trigger Masonry as a callback
-      function( newElements ) {
-        // hide new items while they are loading
-        var $newElems = $( newElements ).css({ opacity: 0 });
-        // ensure that images load before adding to masonry layout
-        $newElems.imagesLoaded(function(){
-          // show elems now they're ready
-          $newElems.animate({ opacity: 1 });
-          $container.masonry( 'appended', $newElems, true ); 
-        });
-      }
-    );
-
-
 });
 
 $('#appendButton').click(function() {
-	var $image = $('<div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
-	$('#container').append( $image ).masonry( 'appended', $image );
+ 	var $image = $('<div class="item"><img src="http://www.phlmetropolis.com/Cats.jpg" width="240px"></div><div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
+ 	$('#container').append( $image ).masonry( 'appended', $image );
+});
+
+$(window).scroll(function () {
+   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+    var $image = $('<div class="item"><img src="http://www.phlmetropolis.com/Cats.jpg" width="240px"></div><div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
+    $('#container').append( $image ).masonry( 'appended', $image );
+   }
 });
