6eeb39130c85d8b31f0a467e8ef6424ad7ecd8d3
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 08:20:44 -0500

    more changes trying to get scroll to work
    this is from last night

--- home/js/main.js
+++ home/js/main.js
@@ -12,12 +12,44 @@ $("#loginAction").click(function() {
 });
 
 $(function(){
+	$container = $('#container');
+
 	console.log("masonry");
-	$('#container').imagesLoaded( function(){
-	  $('#container').masonry({
+	$container.imagesLoaded( function(){
+	  $container.masonry({
 	    // options
 	    itemSelector : '.item',
 	    columnWidth : 240
 	  });
 	});
+
+    $container.infinitescroll({
+      navSelector  : '#page-nav',    // selector for the paged navigation 
+      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
+      itemSelector : '.item',     // selector for all items you'll retrieve
+      loading: {
+          finishedMsg: 'No more pages to load.',
+          img: 'http://i.imgur.com/6RMhx.gif'
+        }
+      },
+      
+      // trigger Masonry as a callback
+      function( newElements ) {
+        // hide new items while they are loading
+        var $newElems = $( newElements ).css({ opacity: 0 });
+        // ensure that images load before adding to masonry layout
+        $newElems.imagesLoaded(function(){
+          // show elems now they're ready
+          $newElems.animate({ opacity: 1 });
+          $container.masonry( 'appended', $newElems, true ); 
+        });
+      }
+    );
+
+
+});
+
+$('#appendButton').click(function() {
+	var $image = $('<div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
+	$('#container').append( $image ).masonry( 'appended', $image );
 });
