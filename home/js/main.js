5e339f8a064c954b88cce1050bf1203a21275b71
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 11:24:06 -0500

    pulling photos.. not sure how

--- home/js/main.js
+++ home/js/main.js
@@ -38,9 +38,11 @@ function photoDataToHTML(photoData){
   return imageHTML;
 };
 
-$(window).scroll(function () {
-   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
-    var $image = $('<div class="item"><img src="http://www.phlmetropolis.com/Cats.jpg" width="240px"></div><div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
-    $('#container').append( $image ).masonry( 'appended', $image );
-   }
-});
+// $(window).scroll(function () {
+//    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 200) {
+
+//     var $image = $('<div class="item"><img src="' + photoDataArray[photoIndex].source + '" width="240px"></div>');
+//     $('#container').append( $image ).masonry( 'appended', $image );
+//     photoIndex++;
+//    }
+// });
