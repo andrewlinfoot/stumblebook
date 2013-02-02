9428ae8b3b6debc97c764ea02bf7356cd65c9286
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 11:59:46 -0500

    finally making progress

--- home/js/main.js
+++ home/js/main.js
@@ -12,31 +12,13 @@ $("#loginAction").click(function() {
 });
 
 $(function(){
-	$container = $('#container');
-
 	console.log("masonry");
-	$container.imagesLoaded( function(){
-	  $container.masonry({
-	    // options
-	    itemSelector : '.item',
-	    columnWidth : 250,
-	  });
-	});
+  $('#container').masonry({
+  // options
+    itemSelector : '.item',
+    columnWidth : 250,
+  });
 });
-
-$('#appendButton').click(function() {
- 	var $image = $('<div class="item"><img src="http://www.phlmetropolis.com/Cats.jpg" width="240px"></div><div class="item"><img src="http://25.media.tumblr.com/tumblr_mbuypt8R0M1ri87b4o1_1280.jpg" width="240px"></div>');
- 	$('#container').append( $image ).masonry( 'appended', $image );
-});
-
-//takes the photoData array and iterates over it, putting it in an html format it in the image format
-function photoDataToHTML(photoData){
-  var imageHTML = '';
-  for( var i=0; i<photoData.length; i++){
-      imageHTML = imageHTML + '<div class="item"><img src="' + photoData[i].url + '" width="240px"></div>'; 
-  }
-  return imageHTML;
-};
 
 // $(window).scroll(function () {
 //    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 200) {
