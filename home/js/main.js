4ce557fb0bd5359ea77bdb4a2186eb32a0957304
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 16:06:28 -0500

    messing with some styles

--- home/js/main.js
+++ home/js/main.js
@@ -11,21 +11,11 @@ $("#loginAction").click(function() {
     }, { scope: 'user_photos,friends_photos' });
 });
 
-$(function(){
-	console.log("masonry");
-  $('#container').masonry({
-  // options
-    itemSelector : '.item',
-    columnWidth : 250,
+//the logout action
+//The logout button
+$("#logoutButton").click(function() {
+  FB.logout(function(response) {
+      // user is now logged out
+      location.reload();
   });
 });
-
-
-// $(window).scroll(function () {
-//    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 200) {
-
-//     var $image = $('<div class="item"><img src="' + photoDataArray[photoIndex].source + '" width="240px"></div>');
-//     $('#container').append( $image ).masonry( 'appended', $image );
-//     photoIndex++;
-//    }
-// });
