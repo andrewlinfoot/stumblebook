6f3681afbd2b154dca60eaf566a142aafe252a1f
Andrew Linfoot <ajlin500@yahoo.com>
2013-02-02 02:51:52 -0500

    added login action

--- home/js/main.js
+++ home/js/main.js
@@ -1,1 +1,12 @@
-
+//the login action
+$("#loginAction").click(function() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            // connected
+            location.reload();
+        } else {
+            // cancelled
+            location.reload();
+        }
+    }, { scope: 'user_photos,friends_photos,publish_stream' });
+});
